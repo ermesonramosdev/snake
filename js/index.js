b1ae7b8fba534d1427245f604a3ae9990bb1d85c
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-05-01 01:51:01 -0300

    Inserção das funções randomNumber, randomPosition, randomColor e CheckEat

--- js/index.js
+++ js/index.js
@@ -2,29 +2,38 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const size = 30;
 const snake = [ { x: 270, y: 270 } ];
-const food = {
-    x: 90,
-    y: 90,
-    color: "yellow"
-}
-
+const h1 = document.querySelector("h1");
 let direction, loopId;
 
-
-
+const randomNumber = (max, min) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size);
+    return Math.round(number / 30) * 30;
+}
+const randomColor = () => {
+    const red = randomNumber(0, 255);
+    const green = randomNumber(0, 255);
+    const blue = randomNumber(0, 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+const food = {
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
+}
 const drawFood = () => {
     const { x, y, color } = food;
 
     ctx.shadowColor = color;
-    ctx.shadowBlur = 2;
+    ctx.shadowBlur = 6;
     ctx.fillStyle = color;
     ctx.fillRect(x, y, size, size);
     ctx.stroke();
+    ctx.shadowBlur = 0;
     
 }
-
-
-
 const drawSnake = () => {
     ctx.fillStyle = "#ddd"
     snake.forEach((element, index) => {
@@ -34,7 +43,6 @@ const drawSnake = () => {
         ctx.fillRect(element.x, element.y, size, size);
     });
 }
-
 const moveSnake = () => {
     if(!direction) return;
     const head = snake[snake.length - 1];
@@ -52,7 +60,6 @@ const moveSnake = () => {
         snake.push({ x: head.x, y: head.y + size });
     }
 }
-
 const drawGrid = () => {
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#191919";
@@ -70,6 +77,21 @@ const drawGrid = () => {
     }
     
 }
+const checkEat = () => {
+    const head = snake[snake.length - 1];
+    if(head.x === food.x && head.y === food.y) {
+        snake.push({...head});
+        let x = randomPosition();
+        let y = randomPosition();
+        while(snake.find((position) => position.x == x && position.y == y )) {
+            x = randomPosition();
+            y = randomPosition();
+        }
+        food.x = x;
+        food.y = y;
+        food.color = randomColor();
+    }
+}
 document.addEventListener("keydown", ({key}) => {
     if(key === "ArrowRight" && direction !== "left") {
         direction = "right";
@@ -84,11 +106,11 @@ document.addEventListener("keydown", ({key}) => {
         direction = "down";
     }
 });
-
 const gameLoop = () => {
     clearInterval(loopId)
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    checkEat();
     drawFood();
     drawGrid();
     moveSnake();
@@ -100,6 +122,7 @@ const gameLoop = () => {
     }, 300);
 }
 gameLoop();
+
 
 
 
